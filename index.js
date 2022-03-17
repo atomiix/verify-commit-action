6373ad42220eb26ace814023407ae124f0e06f97
Thomas BACCELLI <tbaccelli@gmail.com>
2022-03-17 12:18:25 +0100

    test pull request

--- index.js
+++ index.js
@@ -2,17 +2,46 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    /*const octokit = github.getOctokit(core.getInput('token'));
-    const pullRequest = await octokit.rest.pulls.get({
+    if (github.context.issue?.number === undefined) {
+        core.warning('Cannot run the action');
+        return;
+    }
+
+    const token = core.getInput('repo-token', { required: true });
+    const checkMembers = core.getInput('only-organization', { required: true });
+
+    const octokit = github.getOctokit(token);
+
+    const commits = await octokit.paginate(octokit.rest.pulls.listCommits, {
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: github.context.issue.number,
+        per_page: 100
     });
 
-    console.log(pullRequest);*/
+    let membersNames = [];
 
-    console.log(github.context.payload);
-    console.log(github.context.issue);
+    if (checkMembers === 'true') {
+        const allMembers = await octokit.paginate(octokit.rest.orgs.listMembers, {
+            org: github.context.repo.owner,
+            per_page: 100
+        });
+        allMembers.forEach(member => {
+            membersNames.push(member.login);
+        });
+    }
+
+    commits.forEach(({commit, html_url}) => {
+        if (commit.verification.verified === true) {
+            core.info(`Commit ${html_url} is signed.`);
+        } else {
+            if (checkMembers !== 'true' || membersNames.indexOf(commit.author.login)) {
+                core.setFailed(`Commit ${html_url} is not signed.`);
+            } else {
+                core.info(`Commit ${html_url} is not signed. But the author is not part of the organization`);
+            }
+        }
+    });
 }
 
 run();
