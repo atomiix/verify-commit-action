8ed0df44af56f1fcd64a8446a8744d27159949bf
Thomas BACCELLI <tbaccelli@gmail.com>
2022-03-17 12:09:20 +0100

    add repo token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const octokit = github.getOctokit();
+    const octokit = github.getOctokit(core.getInput('token'));
     const pullRequest = await octokit.rest.pulls.get({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
