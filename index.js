895d9ea7756571186f821b05003eb20e7a35d5d7
Thomas BACCELLI <tbaccelli@gmail.com>
2022-03-17 12:11:15 +0100

    test

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const octokit = github.getOctokit(core.getInput('token'));
+    /*const octokit = github.getOctokit(core.getInput('token'));
     const pullRequest = await octokit.rest.pulls.get({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         pull_number: github.context.issue.number,
     });
 
-    console.log(pullRequest);
+    console.log(pullRequest);*/
+
+    console.log(github.context.payload.commits);
 }
 
 run();
